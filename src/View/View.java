3854800f67bd5b7398fbfc3e6a5dae38511df65a
Dilysnguyen <91045422+dilys23@users.noreply.github.com>
2023-04-08 23:22:31 +0700

    Lene

--- src/View/View.java
+++ src/View/View.java
@@ -2,14 +2,21 @@ package View;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class View extends JFrame{
+	private JTextField textField;
 	public View() {
 		getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.setBounds(86, 73, 89, 23);
 		getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(63, 167, 96, 19);
+		getContentPane().add(textField);
+		textField.setColumns(10);
 	}
 	public void ShowWindow()
 	{
