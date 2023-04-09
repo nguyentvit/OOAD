d3b646d59943107dd788ccc4c1f54632410c4593
Nguyên Trần Văn <102210175@sv1.dut.udn.vn>
2023-04-09 10:59:44 +0700

    mei

--- src/View/View.java
+++ src/View/View.java
@@ -2,21 +2,77 @@ package View;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JTextField;
+import com.toedter.calendar.JCalendar;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class View extends JFrame{
+	JButton btnXacNhan;
+	JCalendar calendar;
+	JComboBox cbbTimeStart,cbbTimeEnd;
 	public View() {
 		getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(86, 73, 89, 23);
-		getContentPane().add(btnNewButton);
+		btnXacNhan = new JButton("Xác nh?n");
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(217, 73, 89, 23);
-		getContentPane().add(btnNewButton_1);
+		
+		btnXacNhan.setBounds(245, 270, 89, 23);
+		getContentPane().add(btnXacNhan);
+		
+		 calendar = new JCalendar();
+		calendar.setBounds(10, 10, 272, 202);
+		getContentPane().add(calendar);
+		
+		 cbbTimeStart = new JComboBox();
+		cbbTimeStart.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"}));
+		cbbTimeStart.setBounds(433, 10, 82, 21);
+		getContentPane().add(cbbTimeStart);
+		
+		 cbbTimeEnd = new JComboBox();
+		cbbTimeEnd.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"}));
+		cbbTimeEnd.setBounds(433, 55, 82, 21);
+		getContentPane().add(cbbTimeEnd);
+		
+		JLabel lblNewLabel = new JLabel("Th?i gian b?t d?u:");
+		lblNewLabel.setBounds(311, 14, 110, 13);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Th?i gian k?t thúc:");
+		lblNewLabel_1.setBounds(311, 59, 112, 13);
+		getContentPane().add(lblNewLabel_1);
+		AddEvents();
+		
+		
+	}
+	
+	public void AddEvents()
+	{
+		btnXacNhan.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int timeStart = Integer.parseInt(cbbTimeStart.getSelectedItem().toString());
+				int timeEnd = Integer.parseInt(cbbTimeEnd.getSelectedItem().toString());
+				if(timeStart > timeEnd)
+				{
+					JOptionPane.showMessageDialog(null, "Th?i gian không h?p l?","Thông báo",JOptionPane.INFORMATION_MESSAGE);
+					
+				}
+				else {
+					View1 view1 = new View1();
+					view1.setVisible(true);
+					view1.ShowWindow();
+				}
+				
+			}
+		});
 	}
 	public void ShowWindow()
 	{
+		
 		this.setVisible(true);
 		this.setSize(600, 600);
 		this.setLocationRelativeTo(null);
