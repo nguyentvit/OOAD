3fa4d486c45f4fe136d423c3668c38b11a32d91e
Nguyên Trần Văn <102210175@sv1.dut.udn.vn>
2023-04-09 10:46:53 +0700

    Nguyen1

---
+++ src/View/View1.java
@@ -0,0 +1,16 @@
+package View;
+
+import javax.swing.JFrame;
+
+public class View1 extends JFrame{
+	public View1() {
+		
+	}
+	public void ShowWindow()
+	{
+		this.setSize(500,500);
+		this.setLocationRelativeTo(null);
+		
+	}
+
+}
